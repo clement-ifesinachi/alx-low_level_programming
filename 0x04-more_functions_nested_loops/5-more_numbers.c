080e058364d9e542f857ff3adf25f4dcd4408285
clement-ifesinachi <joeyclement18@gmail.com>
2022-07-09 09:16:31 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * more_numbers-prints number to 14
+ * more_numbers-prints numbers to 14
  *
  * Return: returns nothing
  */
@@ -13,7 +13,7 @@ for (number1 = 0; number1 < 10; number1++)
 for (number2 = 0; number2 <= 14; number2++)
 
 {
-if (number2 < 9)
+if (number2 > 9)
 {
 _putchar((number2 / 10) + '0');
 }
