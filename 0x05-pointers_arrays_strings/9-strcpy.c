ad63980a740c24c5031745e411f447239188703c
clement-ifesinachi <joeyclement18@gmail.com>
2022-07-12 11:43:11 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy _ Copiar el contenido de uno a otra variable
+ * _strcpy - Copiar el contenido de uno a otra variable
  * @dest: This is destiny
  * @src: This is copia
  *
