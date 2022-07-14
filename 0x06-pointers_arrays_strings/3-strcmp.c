9a7a2aa7928d96063d89e15a40f39a140ea3c5ca
clement-ifesinachi <joeyclement18@gmail.com>
2022-07-14 07:51:37 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "maian.h"
+
+/**
+ * _strcmp -> for comparing purpose
+ * @s1: parameter1
+ * @s2: parameter2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s2, s2);
+	return (result);
+}
