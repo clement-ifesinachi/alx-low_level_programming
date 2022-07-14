c3c2819c28d59fadc7898399867df85a70ff98a3
clement-ifesinachi <joeyclement18@gmail.com>
2022-07-14 06:32:31 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet -> a leet function
+ * @str: string parameter
+ * Return: a string
+ */
+char *leet(char *str)
+{
+	int a = 0, b, l = 5;
+	char tr[5] = {'A', 'E', '0', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
+
+	while (str[a])
+	{
+		b = 0;
+
+		while (b < l)
+		{
+			if (str[a] == tr[b] || str[a] - 32 == tr[b])
+				str[a] = trw[b];
+			b++;
+		}
+		a++;
+	}
+	return (str);
+}
