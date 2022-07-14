a76537ebedd701dbc6f8b707916a4872e8ff3d1f
clement-ifesinachi <joeyclement18@gmail.com>
2022-07-14 04:48:31 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncat -> concatenates two strings but addinputted number of bytes
@@ -9,4 +10,6 @@
  */
 char *_strncat(char *dest, char *src, int n);
 {
-
+	strncat(dest, src, n);
+	return (dest);
+}
